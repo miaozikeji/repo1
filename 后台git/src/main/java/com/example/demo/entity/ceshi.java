682d9b30ff5a3561bdda153cb7e8c586700236eb
wangming <1516917637@qq.com>
2020-10-02 10:30:46 +0800

    4

---
+++ 后台git/src/main/java/com/example/demo/entity/ceshi.java
@@ -0,0 +1,15 @@
+package com.example.demo.entity;
+
+public class ceshi {
+    /**
+     *  int 整形  1 2 3 这样子的数字
+     *  double  就是小数   1.5  1.6  2.3  这样
+     *  String 就是字符串  "  这里是任意内容，可以是文字也可以数字   "
+     */
+
+    int jiayi0=0;
+    double jiayi1=1.5;
+    String jiayi2="jiayi";
+
+
+}
